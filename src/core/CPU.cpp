983453960cd3ef87ea8429877f31fb7ea3b10be2
twje <11610634+twje@users.noreply.github.com>
2025-06-21 18:42:01 +1000

    Rename opcode handlers

--- src/core/CPU.cpp
+++ src/core/CPU.cpp
@@ -105,42 +105,42 @@ ExecutionStatus CPU::Execute(const Instruction& instruction)
 
     switch (instruction.GetPatternId())
     {
-        case OpcodePatternId::CLS:           return Execute_CLS(instruction);
-        case OpcodePatternId::RET:           return Execute_RET(instruction);
-        case OpcodePatternId::JP_ADDR:       return Execute_JP_ADDR(instruction);
-        case OpcodePatternId::CALL_ADDR:     return Execute_CALL_ADDR(instruction);
-        case OpcodePatternId::SE_VX_KK:      return Execute_SE_VX_KK(instruction);
-        case OpcodePatternId::SNE_VX_KK:     return Execute_SNE_VX_KK(instruction);
-        case OpcodePatternId::SE_VX_VY:      return Execute_SE_VX_VY(instruction);
-        case OpcodePatternId::LD_VX_KK:      return Execute_LD_VX_KK(instruction);
-        case OpcodePatternId::ADD_VX_KK:     return Execute_ADD_VX_KK(instruction);
-        case OpcodePatternId::LD_VX_VY:      return Execute_LD_VX_VY(instruction);
-        case OpcodePatternId::OR_VX_VY:      return Execute_OR_VX_VY(instruction);
-        case OpcodePatternId::AND_VX_VY:     return Execute_AND_VX_VY(instruction);
-        case OpcodePatternId::XOR_VX_VY:     return Execute_XOR_VX_VY(instruction);
-        case OpcodePatternId::ADD_VX_VY:     return Execute_ADD_VX_VY(instruction);
-        case OpcodePatternId::SUB_VX_VY:     return Execute_SUB_VX_VY(instruction);
-        case OpcodePatternId::SHR_VX_VY:     return Execute_SHR_VX_VY(instruction);
-        case OpcodePatternId::SUBN_VX_VY:    return Execute_SUBN_VX_VY(instruction);
-        case OpcodePatternId::SHL_VX_VY:     return Execute_SHL_VX_VY(instruction);
-        case OpcodePatternId::SNE_VX_VY:     return Execute_SNE_VX_VY(instruction);
-        case OpcodePatternId::LD_I_ADDR:     return Execute_LD_I_ADDR(instruction);
-        case OpcodePatternId::JP_V0_ADDR:    return Execute_JP_V0_ADDR(instruction);
-        case OpcodePatternId::RND_VX_KK:     return Execute_RND_VX_KK(instruction);
-        case OpcodePatternId::DRW_VX_VY_N:   return Execute_DRW_VX_VY_N(instruction);
-        case OpcodePatternId::SKP_VX:        return Execute_SKP_VX(instruction);
-        case OpcodePatternId::SKNP_VX:       return Execute_SKNP_VX(instruction);
-        case OpcodePatternId::LD_VX_DT:      return Execute_LD_VX_DT(instruction);
-        case OpcodePatternId::LD_VX_K:       return Execute_LD_VX_K(instruction);
-        case OpcodePatternId::LD_DT_VX:      return Execute_LD_DT_VX(instruction);
-        case OpcodePatternId::LD_ST_VX:      return Execute_LD_ST_VX(instruction);
-        case OpcodePatternId::ADD_I_VX:      return Execute_ADD_I_VX(instruction);
-        case OpcodePatternId::LD_F_VX:       return Execute_LD_F_VX(instruction);
-        case OpcodePatternId::LD_B_VX:       return Execute_LD_B_VX(instruction);
-        case OpcodePatternId::LD_I_VX:       return Execute_LD_I_VX(instruction);
-        case OpcodePatternId::LD_VX_I:       return Execute_LD_VX_I(instruction);
+        case OpcodePatternId::CLS:         return Execute_00E0_CLS(instruction);
+        case OpcodePatternId::RET:         return Execute_00EE_RET(instruction);
+        case OpcodePatternId::JP_ADDR:     return Execute_1nnn_JP_ADDR(instruction);
+        case OpcodePatternId::CALL_ADDR:   return Execute_2nnn_CALL_ADDR(instruction);
+        case OpcodePatternId::SE_VX_KK:    return Execute_3xkk_SE_VX_KK(instruction);
+        case OpcodePatternId::SNE_VX_KK:   return Execute_4xkk_SNE_VX_KK(instruction);
+        case OpcodePatternId::SE_VX_VY:    return Execute_5xy0_SE_VX_VY(instruction);
+        case OpcodePatternId::LD_VX_KK:    return Execute_6xkk_LD_VX_KK(instruction);
+        case OpcodePatternId::ADD_VX_KK:   return Execute_7xkk_ADD_VX_KK(instruction);
+        case OpcodePatternId::LD_VX_VY:    return Execute_8xy0_LD_VX_VY(instruction);
+        case OpcodePatternId::OR_VX_VY:    return Execute_8xy1_OR_VX_VY(instruction);
+        case OpcodePatternId::AND_VX_VY:   return Execute_8xy2_AND_VX_VY(instruction);
+        case OpcodePatternId::XOR_VX_VY:   return Execute_8xy3_XOR_VX_VY(instruction);
+        case OpcodePatternId::ADD_VX_VY:   return Execute_8xy4_ADD_VX_VY(instruction);
+        case OpcodePatternId::SUB_VX_VY:   return Execute_8xy5_SUB_VX_VY(instruction);
+        case OpcodePatternId::SHR_VX_VY:   return Execute_8xy6_SHR_VX_VY(instruction);
+        case OpcodePatternId::SUBN_VX_VY:  return Execute_8xy7_SUBN_VX_VY(instruction);
+        case OpcodePatternId::SHL_VX_VY:   return Execute_8xyE_SHL_VX_VY(instruction);
+        case OpcodePatternId::SNE_VX_VY:   return Execute_9xy0_SNE_VX_VY(instruction);
+        case OpcodePatternId::LD_I_ADDR:   return Execute_Annn_LD_I_ADDR(instruction);
+        case OpcodePatternId::JP_V0_ADDR:  return Execute_Bnnn_JP_V0_ADDR(instruction);
+        case OpcodePatternId::RND_VX_KK:   return Execute_Cxkk_RND_VX_KK(instruction);
+        case OpcodePatternId::DRW_VX_VY_N: return Execute_Dxyn_DRW_VX_VY_N(instruction);
+        case OpcodePatternId::SKP_VX:      return Execute_Ex9E_SKP_VX(instruction);
+        case OpcodePatternId::SKNP_VX:     return Execute_ExA1_SKNP_VX(instruction);
+        case OpcodePatternId::LD_VX_DT:    return Execute_Fx07_LD_VX_DT(instruction);
+        case OpcodePatternId::LD_VX_K:     return Execute_Fx0A_LD_VX_K(instruction);
+        case OpcodePatternId::LD_DT_VX:    return Execute_Fx15_LD_DT_VX(instruction);
+        case OpcodePatternId::LD_ST_VX:    return Execute_Fx18_LD_ST_VX(instruction);
+        case OpcodePatternId::ADD_I_VX:    return Execute_Fx1E_ADD_I_VX(instruction);
+        case OpcodePatternId::LD_F_VX:     return Execute_Fx29_LD_F_VX(instruction);
+        case OpcodePatternId::LD_B_VX:     return Execute_Fx33_LD_B_VX(instruction);
+        case OpcodePatternId::LD_I_VX:     return Execute_Fx55_LD_I_VX(instruction);
+        case OpcodePatternId::LD_VX_I:     return Execute_Fx65_LD_VX_I(instruction);
     }
-        
+
     return ExecutionStatus::MissingHandler;
 }
 
@@ -157,37 +157,36 @@ bool CPU::IsLegacyInstruction(const Instruction& instruction) const
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_CLS(const Instruction& instruction)
+ExecutionStatus CPU::Execute_00E0_CLS(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_RET(const Instruction& instruction)
+ExecutionStatus CPU::Execute_00EE_RET(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_JP_ADDR(const Instruction& instruction)
+ExecutionStatus CPU::Execute_1nnn_JP_ADDR(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_CALL_ADDR(const Instruction& instruction)
+ExecutionStatus CPU::Execute_2nnn_CALL_ADDR(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SE_VX_KK(const Instruction& instruction)
+ExecutionStatus CPU::Execute_3xkk_SE_VX_KK(const Instruction& instruction)
 {
     const size_t vxIndex = instruction.GetArgument<size_t>(0);
     const uint8_t value = instruction.GetArgument<uint8_t>(1);
     const uint8_t vx = mRegisters.at(vxIndex);
 
-    // Skip next instruction if Vx == kk
     if (vx == value)
     {
         mProgramCounter += INSTRUCTION_SIZE;
@@ -197,19 +196,19 @@ ExecutionStatus CPU::Execute_SE_VX_KK(const Instruction& instruction)
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SNE_VX_KK(const Instruction& instruction)
+ExecutionStatus CPU::Execute_4xkk_SNE_VX_KK(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SE_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_5xy0_SE_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_VX_KK(const Instruction& instruction)
+ExecutionStatus CPU::Execute_6xkk_LD_VX_KK(const Instruction& instruction)
 {
     const size_t vxIndex = instruction.GetArgument<size_t>(0);
     const uint8_t value = instruction.GetArgument<uint8_t>(1);
@@ -219,157 +218,157 @@ ExecutionStatus CPU::Execute_LD_VX_KK(const Instruction& instruction)
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_ADD_VX_KK(const Instruction& instruction)
+ExecutionStatus CPU::Execute_7xkk_ADD_VX_KK(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy0_LD_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_OR_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy1_OR_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_AND_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy2_AND_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_XOR_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy3_XOR_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_ADD_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy4_ADD_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SUB_VX_VY(const Instruction& instruction)
-{    
-    return ExecutionStatus::NotImplemented;
-}
-
-//--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SHR_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy5_SUB_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SUBN_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy6_SHR_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SHL_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xy7_SUBN_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SNE_VX_VY(const Instruction& instruction)
+ExecutionStatus CPU::Execute_8xyE_SHL_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_I_ADDR(const Instruction& instruction)
+ExecutionStatus CPU::Execute_9xy0_SNE_VX_VY(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_JP_V0_ADDR(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Annn_LD_I_ADDR(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_RND_VX_KK(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Bnnn_JP_V0_ADDR(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_DRW_VX_VY_N(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Cxkk_RND_VX_KK(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SKP_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Dxyn_DRW_VX_VY_N(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_SKNP_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Ex9E_SKP_VX(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_VX_DT(const Instruction& instruction)
+ExecutionStatus CPU::Execute_ExA1_SKNP_VX(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_VX_K(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Fx07_LD_VX_DT(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_DT_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Fx0A_LD_VX_K(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_ST_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Fx15_LD_DT_VX(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_ADD_I_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Fx18_LD_ST_VX(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_F_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Fx1E_ADD_I_VX(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_B_VX(const Instruction& instruction)
+ExecutionStatus CPU::Execute_Fx29_LD_F_VX(const Instruction&)
 {
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_I_VX(const Instruction& instruction)
-{ 
+ExecutionStatus CPU::Execute_Fx33_LD_B_VX(const Instruction&)
+{
     return ExecutionStatus::NotImplemented;
 }
 
 //--------------------------------------------------------------------------------
-ExecutionStatus CPU::Execute_LD_VX_I(const Instruction& instruction)
-{    
+ExecutionStatus CPU::Execute_Fx55_LD_I_VX(const Instruction&)
+{
+    return ExecutionStatus::NotImplemented;
+}
+
+//--------------------------------------------------------------------------------
+ExecutionStatus CPU::Execute_Fx65_LD_VX_I(const Instruction&)
+{
     return ExecutionStatus::NotImplemented;
 }
